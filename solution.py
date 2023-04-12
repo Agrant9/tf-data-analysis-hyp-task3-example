12cb2d15d986d26869c8e3b602370de62726febf
Agrant9 <96260635+Agrant9@users.noreply.github.com>
2023-04-12 21:42:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
 
 chat_id = 544835691 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, pval = stats.ks_2samp(x, y, alternative='less')
+    return True if pval < 0.07 else False # Ваш ответ, True или False
